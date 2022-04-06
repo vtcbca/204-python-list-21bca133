70b2c41b9b878eccc8b7565d383e829d2bf3bdf7
21bca133 <100265459+21bca133@users.noreply.github.com>
2022-04-06 20:33:43 +0530

    Delete solution2.py

--- solution2.py
+++
@@ -1,5 +0,0 @@
-#WAS to enter kilometer and converted it into meter.
-km=float(input("enter a value of kilometer: "))
-meter=1000
-conmt=km*meter
-print("{} convert into meter = {}".format(km,conmt))
